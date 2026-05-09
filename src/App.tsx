313b3f1b1ba2baa2ea89e400d5d46af5aacc70f3
ramkrishna0x0 <ramakrishnasahoo001@gmail.com>
2026-05-10 01:33:49 +0530

    Feat: Integrate Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Home from './pages/Home';
 import Download from './pages/Download';
 import { AnimatedCursor } from './components/AnimatedCursor';
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/d/:id" element={<Download />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
